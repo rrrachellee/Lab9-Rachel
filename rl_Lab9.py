fb9ffe83eb37fa4ebfdc30afe87d2410e445bf9d
kalinatringas <kalinatringas@ufl.edu>
2024-04-13 18:09:13 -0400

    decoder

--- rl_Lab9.py
+++ rl_Lab9.py
@@ -1,1 +1,7 @@
-dd
+def decode (passC):
+    #already should be an integer at this point
+    decoded = []
+    for i in range (len(passC)):
+        meow = passC[i] - 3
+        decoded.append(meow)
+    return decoded 
