83861f63d5d5604237cb8193da46ba97a95f0cf9
rrrachellee <166664893+rrrachellee@users.noreply.github.com>
2024-04-13 20:40:00 -0400

    Update rl_Lab9.py

--- rl_Lab9.py
+++ rl_Lab9.py
@@ -18,7 +18,7 @@ while True:
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-    choice = input("Please enter an option: ")
+    choice = input("\nPlease enter an option: ")
 
     if choice == "1":
         password = input("Please enter your password to encode: ")
